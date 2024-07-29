647ce4d2e14d715fdd5e17814dbbe06692709dfa
Antunes <36316038+zpdh@users.noreply.github.com>
2024-07-29 04:38:04 -0300

    feat: Adding seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,17 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
+
+        Department d = new Department(1, "D1");
+        Seller s = new Seller(1, "name", "example@gmail.com", new Date(), 3000.0, d);
+        
+        System.out.println(d);
+        System.out.println(s);
     }
 }
