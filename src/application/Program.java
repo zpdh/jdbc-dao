2058da3ba56cc0f4936136dde4d99c2495ad0fae
Antunes <36316038+zpdh@users.noreply.github.com>
2024-07-29 04:43:51 -0300

    feat: Adding seller and department DAO interfaces

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,7 +10,7 @@ public class Program {
 
         Department d = new Department(1, "D1");
         Seller s = new Seller(1, "name", "example@gmail.com", new Date(), 3000.0, d);
-        
+
         System.out.println(d);
         System.out.println(s);
     }
