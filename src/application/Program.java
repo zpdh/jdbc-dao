31e57950c6a2ce14352c6b895d242006e6ae67c3
Antunes <36316038+zpdh@users.noreply.github.com>
2024-07-29 04:50:02 -0300

    feat: Adding SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,6 +12,8 @@ public class Program {
 
         Department d = new Department(1, "D1");
         Seller s = new Seller(1, "name", "example@gmail.com", new Date(), 3000.0, d);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(d);
         System.out.println(s);
